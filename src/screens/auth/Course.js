b1ce3b55f792289a597fce3de50b8f93e2911932
Jonathan Corral <johnlejardinnier@gmail.com>
2018-03-12 19:08:11 +0100

    fix(app): update step and add finish course

--- src/screens/auth/Course.js
+++ src/screens/auth/Course.js
@@ -10,26 +10,32 @@ export default class Course extends React.Component {
 
         this.state = {
             isLoading: false,
-            user:'',
-            preparation:'',
-            course:'',
+            user:false,
+            preparation:false,
+            course:false,
             message:'',
             step:0,
+            finished:false,
         }
     }
 
 
     componentDidMount() {
         // load user from storage
+        this.setState({isLoading:true})
         console.log("loading user...", storage)
         storage.load({
             key: 'user',
         }).then(data => {
-            this.setState({user:data})
             console.log("loaded user from storage")
+            this.setState({
+                user:data,
+                isLoading:false,
+            })
         }).catch(err => {
             console.log('key NOT found in storage !')
             console.warn(err.message);
+            this.setState({isLoading:true})
         })
     }
 
@@ -67,7 +73,7 @@ export default class Course extends React.Component {
                 }
             })
             .then(() => {
-                this.setState({ isLoading: false})
+                //this.setState({ isLoading: false})
             })
             .catch(err => {
                 this.setState({ message: err.message })
@@ -95,13 +101,14 @@ export default class Course extends React.Component {
             .then((response) => {
                 console.log(response);
 
-                if (response.message == undefined) {
+                if (response.message != undefined) {
+                    this.setState({ message: response.message })
+                }
+                else {
+                    // on success
                     this.setState({
                         course:response
                     })
-                }
-                else {
-                    this.setState({ message: response.message })
                 }
             })
             .then(() => {
@@ -124,9 +131,43 @@ export default class Course extends React.Component {
 
     _validateStep = () => {
         console.log("validating current step...");
-        if(this.state.step < this.state.course.length) {
-            this.setState({isLoading:true, step: this.state.step + 1});
-            this.setState({isLoading: false});
+        if(this.state.step < this.state.course.length -1) {
+            this.setState({isLoading:true});
+            //setTimeout(() => {this.setState({isLoading:false})}, 500)
+
+            let params = {
+                stepValidated:true
+            };
+
+            fetch(GLOBAL.BASE_URL + "courses/update/" + this.state.course[this.state.step].id, {
+                method: GLOBAL.METHOD.PATCH,
+                headers: {
+                    Accept: GLOBAL.CONTENT_TYPE.JSON,
+                    'Content-Type': GLOBAL.CONTENT_TYPE.JSON,
+                    'X-Auth-Token':this.state.user.jwt
+                },
+                body: JSON.stringify(params)
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    console.log(response);
+
+                    if (response.message != undefined) {
+                        this.setState({ message: response.message })
+                    }
+                    else {
+                        //on success
+                        this.setState({step: this.state.step + 1});
+                    }
+                })
+                .then(() => {
+                    this.setState({ isLoading: false})
+                })
+                .catch(err => {
+                    this.setState({ message: err.message })
+                    this.setState({ isLoading: false })
+                });
+
         } else {
             this._finishCourse();
         }
@@ -135,9 +176,44 @@ export default class Course extends React.Component {
 
     _finishCourse = () => {
         console.log("finishing course...");
-        this.setState({isLoading: true});
-        this.setState({course: ''});
-        this.setState({isLoading: false});
+        this.setState({
+            isLoading: true,
+            course:false
+        });
+        //setTimeout(() => {this.setState({isLoading:false})}, 1000)
+
+        fetch(GLOBAL.BASE_URL + "preparations/finish/" + this.state.preparation.id, {
+            method: GLOBAL.METHOD.GET,
+            headers: {
+                Accept: GLOBAL.CONTENT_TYPE.JSON,
+                'Content-Type': GLOBAL.CONTENT_TYPE.JSON,
+                'X-Auth-Token':this.state.user.jwt
+            },
+            //body: JSON.stringify(params)
+        })
+            .then((response) => response.json())
+            .then((response) => {
+
+                if (response.message != undefined) {
+                    this.setState({ message: response.message })
+                }
+                else {
+                    // reset data
+                    this.setState({
+                        preparation:false,
+                        course:false,
+                        finished:true,
+                    })
+                }
+            })
+            .then(() => {
+                this.setState({ isLoading: false})
+            })
+            .catch(err => {
+                this.setState({ message: err.message })
+                this.setState({ isLoading: false })
+            });
+
     }
 
 
@@ -151,7 +227,7 @@ export default class Course extends React.Component {
                 </View>
             )
         } else {
-            if(!this.state.course) {
+            if((!this.state.preparation || !this.state.course) && !this.state.finished) {
                 return (
                     <View style={style.container}>
                         <Text style={[style.h1, style.centerAlign]}>Course</Text>
@@ -163,8 +239,6 @@ export default class Course extends React.Component {
                             title={"Commencer"}
                             onPress={() => this._startCourse()} />
 
-
-                        {this.state.isLoading && <ActivityIndicator size={"large"} color={"#0099ff"} />}
                         {!!this.state.message && (
                             <Text style={[style.text, style.centerAlign, style.error]}>
                                 {this.state.message}
@@ -172,7 +246,7 @@ export default class Course extends React.Component {
                         )}
                     </View>
                 )
-            } else {
+            } else if(this.state.preparation && this.state.course && !this.state.finished) {
                 const{step} = this.state;
                 return(
                     <ScrollView style={style.container}>
@@ -184,13 +258,13 @@ export default class Course extends React.Component {
                                 <Text>{step + 1} / {this.state.course.length}</Text>
                             </Text>
 
-                            <Text style={[style.text, style.big]}>
+                            {this.state.course[step].quantity && <Text style={[style.text, style.big]}>
                                 <Text style={[style.bold]}>Quantité : </Text>
                                 <Text>{this.state.course[step].quantity}</Text>
-                            </Text>
+                            </Text>}
                         </View>
 
-                        <View style={style.container}>
+                        {this.state.course[step].productPosition.position && <View style={style.container}>
                             <Text style={[style.h2]}>{"\n"}Localisation</Text>
                             <Text style={[style.text]}>
                                 <Text style={[style.bold]}>Allée : </Text>
@@ -202,9 +276,9 @@ export default class Course extends React.Component {
                                 <Text style={[style.bold]}> - Etagère : </Text>
                                 <Text>{this.state.course[step].productPosition.position.shelf}</Text>
                             </Text>
-                        </View>
+                        </View>}
 
-                        <View style={style.container}>
+                        {this.state.course[step].productPosition.product && <View style={style.container}>
                             <Text style={[style.h2]}>{"\n"}Informations</Text>
                             <Text style={[style.text]}>
                                 <Text style={[style.bold]}>Poids : </Text>
@@ -214,7 +288,7 @@ export default class Course extends React.Component {
                                 <Text style={[style.bold]}>EAN13 : </Text>
                                 <Text>{this.state.course[step].productPosition.product.ean13}</Text>
                             </Text>
-                        </View>
+                        </View>}
 
                         <View style={style.container}>
                             <Text style={[style.h2]}>{"\n"}Actions</Text>
@@ -234,7 +308,6 @@ export default class Course extends React.Component {
                                 title={"Valider cette étape"}
                                 onPress={() => this._validateStep()} />
 
-                            {this.state.isLoading && <ActivityIndicator size={"large"} color={"#0099ff"} />}
                             {!!this.state.message && (
                                 <Text style={[style.text, style.centerAlign, style.error]}>
                                     {this.state.message}
@@ -243,6 +316,19 @@ export default class Course extends React.Component {
                             <Text>{"\n"}{"\n"}</Text>
                         </View>
                     </ScrollView>
+                )
+            } else {
+                return (
+                    <View style={style.container}>
+                        <Text style={[style.h1, style.centerAlign]}>Course</Text>
+                        <Text style={[style.text]}>Bravo {this.state.user.name},{"\n"}</Text>
+                        <Text style={[style.text]}>Vous avez terminé la course !{"\n"}{"\n"}</Text>
+                        <Text style={[style.text, style.info]}>
+                            Veuillez maintenant vous diriger vers le service ventilation
+                            avec votre caddie.{"\n"}
+                            Merci.
+                        </Text>
+                    </View>
                 )
             }
         }
